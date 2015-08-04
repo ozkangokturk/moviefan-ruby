a06a50ea9e9b581b2097e03bb270e01f11aa1428
ozkangokturk <ozkangokturk@gmail.com>
2015-08-04 21:42:55 +0300

    langKey added for user model

--- db/migrate/20150804101933_add_first_last_name_to_users.rb
+++ db/migrate/20150804101933_add_first_last_name_to_users.rb
@@ -2,5 +2,6 @@ class AddFirstLastNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :firstName, :text
     add_column :users, :lastName, :text
+    add_column :users, :langKey, :text
   end
 end
